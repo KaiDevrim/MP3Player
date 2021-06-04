82ab45fefb4fd249cfd7a8c469e363b684c58978
MyGuy11 <35243044+MyGuy11@users.noreply.github.com>
2021-06-04 12:55:15 -0700

    Revert "."
    This reverts commit 7a9df7ab3d1724839e3cb3ab57af4b15951aaa99.

--- src/AudioPlayer.java
+++ src/AudioPlayer.java
@@ -1,3 +1,4 @@
+import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaPlayer.Status;
@@ -33,7 +34,7 @@ public class AudioPlayer
     public void playAudio()
     {
         players.get(quePos).play();
-        System.out.println(players.get(quePos).getMedia().getDuration().toSeconds());
+        System.out.println(players.get(quePos).getMedia().getDuration());
     }
     
     public void pauseAudio()
@@ -66,10 +67,8 @@ public class AudioPlayer
         for (int i = 0; i < players.size(); i++)
         {
             quePos = i;
-            players.get(quePos).play();
-            while (players.get(quePos).getStatus().equals(Status.PLAYING))
-            {
-            }
+            players.get(i).play();
+            //wait(players.get(quePos).getCycleDuration());
         }
     }
 
